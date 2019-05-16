8afc174637460418c8d2a0248b0d30f07f75e6ad
marabesi <matheus.marabesi@gmail.com>
2019-05-16 13:24:11 +0200

    added service-worker folder

---
+++ service-worker/hijack-1/hijack-1-sw.js
@@ -0,0 +1,5 @@
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    new Response('hello')
+  )
+})
---
+++ service-worker/hijack-2/hijack-2-sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  )
+})
