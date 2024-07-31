3b2ecf1ff719dd22a12682cc2a27fd5bd8d36b13
marabesi <matheus.marabesi@gmail.com>
2024-07-31 06:48:14 +0200

    chore(three.js): orbit controls

---
+++ threejs/orbit-controls/README.md
@@ -0,0 +1,3 @@
+# Orbit controls
+
+- [https://threejs.org/docs/#examples/en/controls/OrbitControls](https://threejs.org/docs/#examples/en/controls/OrbitControls)
---
+++ threejs/orbit-controls/main.js
@@ -0,0 +1,28 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(2, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.set(0, 20, 100);
+controls.update();
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+
+document.body.appendChild(renderer.domElement);
