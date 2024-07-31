fe2704c1221af9a7bb7e4338e6522af36a6a0073
marabesi <matheus.marabesi@gmail.com>
2024-07-31 06:52:00 +0200

    chore(three.js): orbit controls

--- threejs/orbit-controls/README.md
+++ threejs/orbit-controls/README.md
@@ -1,3 +1,4 @@
 # Orbit controls
 
 - [https://threejs.org/docs/#examples/en/controls/OrbitControls](https://threejs.org/docs/#examples/en/controls/OrbitControls)
+- [Your Guide To The Orbit Controls Module In Three.js](https://www.youtube.com/watch?v=Nxd9L6X8quo)
--- threejs/orbit-controls/main.js
+++ threejs/orbit-controls/main.js
@@ -5,6 +5,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(2, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
