73fb0f27009cc01fb90371180ee03b72294f570f
marabesi <matheus.marabesi@gmail.com>
2024-08-04 17:07:18 +0200

    chore(three.js): texture map

--- threejs/texture-map/main.js
+++ threejs/texture-map/main.js
@@ -1,52 +1,35 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-function createMaterial() {
-  // create a texture loader.
+function createCubeWithMaterial() {
   const textureLoader = new THREE.TextureLoader();
-
-  // load a texture
   const texture = textureLoader.load(
     'textures/dirt.png',
   );
 
-  // create a "standard" material using
-  // the texture we just loaded as a color map
   const material = new THREE.MeshBasicMaterial({
-    // color: 0x00ff00,
     map: texture,
-    // wireframe: true,
   });
-  // const material = new THREE.MeshStandardMaterial({
-  //   map: texture,
-  //   color: 'purple'
-  // });
 
-  return material;
+  return new THREE.Mesh(geometry, material);
 }
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  // 2, window.innerWidth / window.innerHeight, 0.1, 1000
- 35, // fov = Field Of View
+  35, // fov = Field Of View
   1, // aspect ratio (dummy value)
   0.1, // near clipping plane
   100, // far clipping plane
 );
+
 const renderer = new THREE.WebGLRenderer();
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-// geometry.isBufferGeometry = true;
 
-const material = createMaterial();
+const cube = createCubeWithMaterial();
 
-// const material = new THREE.MeshStandardMaterial({ color: 'purple' });
-// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-//material.wireframe = true;
-
-const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.set(0, 0, 10);
